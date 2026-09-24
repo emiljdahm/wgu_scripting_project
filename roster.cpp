53343c6dd315669f9e1cded4597b2e9c8b474e44
Emil Dahm II <emildahmii@Emils-MBP.lan>
2026-09-23 18:41:58 -0700

    updated roster cpp

--- roster.cpp
+++ roster.cpp
@@ -1,5 +1,43 @@
 #include "roster.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
 
+int classRosterArray[] = {}; // holds studentData table
+
+int* arrPtr = classRosterArray;
+
+class Roster {
+    private:
+
+    public:
+    void add(std::string studentID,
+        std::string firstName,
+        std::string lastName,
+        std::string email,
+        int age,
+        int daysInCourse1,
+        int daysInCourse2,
+        int daysInCourse3,
+        enum DegreeProgram degreeProgram
+    ){
+        //add user details 
+    };
+    void remove(){
+        //remove student based on ID, if !== id then skip, if no id exist, print error
+    };
+    void printAll(){
+        //loop through all students and call print()
+    };
+    void printAverageDaysInCourse(){
+        //print avg days in three courses based on student ID
+    };
+    void printInvalidEmails(){
+        // loop throug all emails and print any invalid emails
+    };
+    void printByDegreeProgram(DegreeProgram degreeProgram){
+        //print out enum data type for degree program
+    }
+
+};
